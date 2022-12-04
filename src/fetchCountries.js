3fe25fd7b49ec4d1cddb15dfb3a89678614b03d3
Irfnekf <vladshkaraputa07@gmail.com>
2022-12-04 14:25:05 +0200

    final

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,60 +1,11 @@
-import Notiflix from 'notiflix';
-import { refs } from './index';
-
-const elem = [];
-
-export function fetchCountries(country) {
+export function fetchCountries(name) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      elem === data;
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
 
-      if (elem.status === 404) {
-        throw error;
-      }
-      if (data.length > 10) {
-        return Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      }
-      if (data.length >= 2 || data.length < 10) {
-        renderList();
-      }
-      // if (data.length === 1) {
-      //   // createDivCountres();
-      // }
-    })
-    .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
-}
-
-function renderList(elem) {
-  elem.reduce((acc, { name: { official }, flags: { svg } }) => {
-    return (
-      acc +
-        `<li><img src="${svg}" alt="flags" width="40" /><span>${official}</span></li>`,
-      ''
-    );
+    return response.json();
   });
 }
-
-function createListCountres() {
-  refs.ul.innerHTML = '';
-  refs.ul.insertAdjacentElement('beforeend', renderList);
-}
-// function listCountres({ name: { official }, flags: { svg } }) {
-//   return `<li><img src="${svg}" alt="flags" width="40" /><span>${official}</span></li>`;
-// }
-
-// function divCountres({ name, flags, capital, population, languages }) {}
-// function createDivCountres() {}
-
-// function createListCountres() {
-//   // const list = elem.map(listCountres);
-//   refs.ul.innerHTML = '';
-//   refs.ul.insertAdjacentElement('beforeend', list.join(''));
-// }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,63 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-export const refs = {
+const refs = {
   input: document.querySelector('#search-box'),
   ul: document.querySelector('.country-list'),
   div: document.querySelector('.country-info'),
 };
+let elem = '';
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-function onInput(e) {
-  let country = e.target.value.trim();
-  if (country === '') {
+function onInput() {
+  if (!refs.input.value.trim()) {
     refs.ul.innerHTML = '';
-    refs.div.innerHTML = '';
     return;
   }
-  fetchCountries(country);
+  fetchCountries(refs.input.value.trim())
+    .then(createCounries)
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure(`Oops, there is no country with that name`);
+      refs.ul.innerHTML = '';
+    });
+}
+function createCounries(countries) {
+  if (countries.length >= 2 && countries.length <= 10) {
+    renderList(countries);
+    refs.div = '';
+  } else if (countries.length === 1) {
+    renderDiv(countries);
+    refs.div.innerHTML = '';
+  } else if (countries > 10) {
+    refs.ul.innerHTML = '';
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+  refs.ul.innerHTML = elem;
+}
+
+function renderList(countries) {
+  elem = countries
+    .map(({ name: { official }, flags: { svg } }) => {
+      return `<li class = "country-items"><img src="${svg}" alt="flags" width="40" /><span class="country-span">${official}</span></li>`;
+    })
+    .join('');
+}
+function renderDiv(countries) {
+  elem = countries.map(country => {
+    return `<li class = "country-items">
+    <img src="${country.flags.svg}" width = 40 alt="" />
+    <span>${country.name.official}</span>
+  </li>
+  <div>Capital: ${country.capital}</div>
+  <div>Population: ${country.population}</div>
+  <div>Languages: ${Object.values(country.languages).join(`, `)}</div>`;
+  });
 }
